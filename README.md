b87079a3358df723adba8a28053586635ebbb596
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-10-05 13:33:58 -0500

    ***

---
+++ README.md
@@ -0,0 +1,9 @@
+##### Convert 15char Salesforce Id to 18char
+
+```shel
+c++ sfid.cpp -o sfid
+```
+```shel
+./sfid 006C000000jjKuE
+#=> 006C000000jjKuEIAU
+```
---
+++ sfid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+#include <bitset>
+#include <algorithm>
+
+#define BASE "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456"
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+  string id = argv[1];
+  string chars;
+  string checksum = "";
+
+  for (int i = 0; i < 3; ++i) {
+    chars = id.substr(i * 5, 5);
+    string binary = "";
+    stringstream ss;
+
+    for (char & c : chars) {
+      ss << isupper(c);
+    }
+
+    binary = ss.str();
+    reverse(binary.begin(), binary.end());
+    bitset<5> x(binary);
+
+    checksum += BASE[x.to_ulong()];
+  }
+  cout << id.substr(0, 15) << checksum << endl;
+  return 0;
+}
